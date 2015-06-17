591deeb43945c0b29aaf40559a3a1a65a512d6e1
vvo <vincent.voyer@algolia.com>
2015-06-17 11:25:06 +0200

    fix: wrap every diagnostic in a promise to get uncaught errors
    SO that we show uncaught errors in the diagnostic

--- src/diag.js
+++ src/diag.js
@@ -22,7 +22,7 @@ function diag() {
 
   // diagnostic is a function here, it was required earlier
   function addToQueue(diagnostic) {
-    jobs.push(diagnostic);
+    jobs.push(promiseWrap(diagnostic));
   }
 
   // `job` is a function, it's one of the diagnostic
@@ -37,6 +37,7 @@ function diag() {
 
   // `job` is a function, it's one of the diagnostic
   // we can access the function name with job.name if needed
+  // as we wrap everything in a promise to display errors we should never get here
   jobs.once('error', function(err, job) {
     emitter.emit('error', err, job);
     jobs.stop();
@@ -54,4 +55,42 @@ function diag() {
 
 function requireIt(file) {
   return require('./diagnostics/' + file);
+}
+
+// we wrap in Promise so that any uncaught will.. get caught
+// and we can show it
+function promiseWrap(diagnostic) {
+  return function(cb) {
+    var promise = new Promise(function(resolve, reject) {
+      diagnostic(function(err, dataset) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(dataset);
+      });
+    });
+
+    promise
+      .then(function(dataset) {
+        cb(null, dataset);
+      })
+      .catch(function(err) {
+        err = formatDatasetFromError(err, diagnostic.name);
+
+        cb(null, err);
+      });
+  };
+}
+
+function formatDatasetFromError(err, diagnosticName) {
+  return {
+    title: 'ERROR: ' + diagnosticName,
+    header: ['error.message', 'error.stack'],
+    data: [[
+      err.message,
+      err.stack && err.stack.toString() || 'no stack information'
+    ]]
+  };
 }
