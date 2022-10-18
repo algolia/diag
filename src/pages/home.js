3b572fdaf332d1e25b9b357b53917536a230261d
Tatsuro Handa <2632519+t2hnd@users.noreply.github.com>
2022-10-18 20:49:39 +0900

    feat: Update index page to fetch params from query strings (#40)
    * Update index page to fetch params from query strings
    
    * Move prefill function to src/home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,4 +2,17 @@ module.exports = home;
 
 function home() {
   $('input[name="t"]').val(Date.now());
+  prefillAdvancedOptions();
+
 }
+
+function prefillAdvancedOptions() {
+  var formParameters = ['applicationId', 'apiKey', 'indexName'];
+  var params = new URL(document.location).searchParams;
+
+  for (var idx in formParameters){
+    if (params.has(formParameters[idx])){
+      document.getElementById(formParameters[idx]).value = params.get(formParameters[idx]);
+    }
+  }
+ }
